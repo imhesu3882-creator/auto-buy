e878c1883cae4c571421ba82aebc2e7517c3deca
imhesu3882-creator <imhesu3882@naver.com>
2026-06-16 03:07:10 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,4 @@
 BASE_URL = "https://openapivts.koreainvestment.com:29443"
-START_BALANCE = 10_000_000
 APP_KEY = "PSJbO5Jd83iOL0JbEeg13LWg58OvdIobJorc"
 APP_SECRET = "yCcCYagqkZpkvMxvQtIOXFG5mp1nGXTu47cGD+5UObiX/0PjtWX1ViRvKMgaMkY1nXBa7NM8nTaR82nhgwomwunBmZOVRAvx6rfKirR87HoZajoM2wLAh36Wl7zIaU00YUBG2IBRzePyERcOWjXyJ2Qo1ID6CHRNj6leqvTqgp9NhdOc1cA="
-
 STOCKS = {"삼성전자": "005930", "SK하이닉스": "000660", "삼성전기": "009150", "NAVER": "035420"}
-TICKERS = {"삼성전자": "005930.KS", "SK하이닉스": "000660.KS", "삼성전기": "009150.KS", "NAVER": "035420.KS"}
-
-REFRESH_SECONDS = 5
-BUY_SCORE = 65
-SELL_SCORE = 35
